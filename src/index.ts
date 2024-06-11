c7498c05740300d20343a23b6d83f35a1e9572ec
xiarx <xiarx@protonmail.com>
2024-06-11 20:45:12 +0200

    setup (#1)

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+
+import server from './server.js'
+
+server.listen(process.env.PORT)
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+import type {Express} from 'express'
+
+const server: Express = express()
+
+export default server
